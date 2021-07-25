9fd60f3e6264d6a2eaffbab3adccc467612d4908
unknown <gabriel.chimenezleme@gmail.com>
2021-07-25 18:20:33 -0300

    adding docker-compose and increase email code base

--- ms-pet-email-listener/src/main/java/com/fiap/pet/mspetemaillistener/RabbitMQListener.java
+++ ms-pet-email-listener/src/main/java/com/fiap/pet/mspetemaillistener/RabbitMQListener.java
@@ -3,15 +3,29 @@ package com.fiap.pet.mspetemaillistener;
 import com.fiap.pet.mspetemaillistener.dto.EmailEnum;
 import com.fiap.pet.mspetemaillistener.dto.RabbitListenerEmailDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class RabbitMQListener {
 
+    @Autowired
+    private SendgridSender sgs;
+
+
     @RabbitListener(queues="${rabbit.queuename}", containerFactory="jsaFactory")
-    public void receivedMessage(RabbitListenerEmailDto message) {
+    public void receivedMessage(RabbitListenerEmailDto message) throws IOException {
         if(message.getTipoEmail().equals(EmailEnum.EMAIL_PEDIDO)) {
-            //envia email pedido
+            sgs.setEmailFrom("pet@teste.com");
+            sgs.setEmailTo("gabriel.chimenezleme@gmail.com");
+            sgs.setSubject("Email de teste!!");
+            try {
+                sgs.sendEmail();
+            } catch (Exception e) {
+                throw e;
+            }
         }
         else if(message.getTipoEmail().equals(EmailEnum.EMAIL_CADASTRO)) {
             //envia email cadastro
